fc2e4efc8d2ff1e8af744a680c93105a5f1da3a2
dandandujie <142396650+dandandujie@users.noreply.github.com>
2023-08-20 14:58:21 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 window.onload = function() {
     // 在这里获取你的哔哩哔哩视频推荐和链接，并将它们展示在你的网站上
 }
+function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    if (sidebar.style.transform === 'translateX(100%)') {
+        sidebar.style.transform = 'translateX(0)';
+    } else {
+        sidebar.style.transform = 'translateX(100%)';
+    }
+}
